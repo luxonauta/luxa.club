10ae9eac9dc8e3c9729281fe12d6558a06aba91d
Lucas de França <luxon4uta@gmail.com>
2024-07-30 14:34:51 -0300

    🧰 Fix Leaderboard & adjust table styles

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-const getLeaderboard = React.lazy(() => import("@/utils/supabase/actions").then(module => ({ default: module.getLeaderboard })));
+import { getLeaderboard } from "@/utils/supabase/actions";
 import "./index.scss";
 
 const Leaderboard = () => {
